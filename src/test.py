176edd390cfdf8a9438d19110fd342cabdc389c0
VasilisTsimpos <vasilis02013@gmail.com>
2024-07-25 14:51:29 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,35 +7,67 @@ import matplotlib.pyplot as plt
 from tools import *
 
 if __name__ == "__main__":
+    robot = Jackal(10)
+    time = 0
+    position_log = []
+    time_log = []
+    v_log = []
+    velocity_log = []
+
+    rate = robot.getRate()
+        
+    #Robots orientation
+    R0b = robot.getRotationMatrix()
+        
+    #Robots position from inertial frame
+    P0b = robot.getPosition()
+
+    #Robots desired position from robots frame
+    Pd = np.array([1.5, 1.0, 0.0])
+    #Robots desired position from inertial frame
+    P0d = P0b + changePerspective(Pd, R0b)
+
     try:
-        robot = Jackal(10)
-        time = 0
-
-        rate = robot.getRate()
-        
-        #Robots orientation
-        R0b = robot.getRotationMatrix()
-        
-        #Robots position from inertial frame
-        P0b = robot.getPosition()
-
-        #Robots desired position from robots frame
-        Pd = np.array([1.0, 1.0, 0.0])
-        #Robots desired position from inertial frame
-        P0d = P0b + changePerspective(Pd, R0b)
-
         while not rospy.is_shutdown():
-            e = getError(robot.getPosition(), P0d, robot.getRotationMatrix()) # From robots frame
-            
-            lin_vel = 1 * getLinearError(e)
-            ang_vel = 0.5 * getAngularError(e)
+            pos = robot.getPosition()
 
-            robot.setLinearSpeed(lin_vel)
+            e = getError(pos, P0d, robot.getRotationMatrix()) # From robots frame
+            
+            pd, v, a = getFifthOrder(time, 8, P0b, 0, 0, P0d, 0, 0)
+            v = np.linalg.norm(v)
+            a = np.linalg.norm(a)
+
+            ang_vel = 0.7 * getAngularError(e)            
+            if np.linalg.norm(e[:2]) < 0.05:
+                ang_vel = 0
+                raise Exception
+
+            robot.setLinearSpeed(v)
             robot.setAngularSpeed(ang_vel)
-            robot.setRobotSpeed()   
+            robot.setRobotSpeed()
+
+            velocity = robot.calcLinearVel()
 
             time += 0.1
+
+            #Log Data
+            velocity_log.append(velocity)
+            position_log.append(pos)
+            v_log.append(v)
+            time_log.append(time)
+
             rate.sleep()
             
     except rospy.ROSInterruptException:
         pass
+
+    except Exception:
+        print('Last Position: ', pos[:2])
+        plt.figure()
+        plt.plot(np.array(time_log), np.array(v_log))
+        plt.plot(np.array(time_log), np.array(velocity_log))
+        plt.xlabel('Time')
+        plt.ylabel('Velocity')
+        plt.legend(['Commanded velocity', 'Calculated velocity'])
+        plt.grid()
+        plt.show()
