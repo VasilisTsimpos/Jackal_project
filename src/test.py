c2d9a84c16219546746a58fe73cb2f4040e535ba
VasilisTsimpos <vasilis02013@gmail.com>
2024-07-25 16:55:23 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -9,7 +9,9 @@ from tools import *
 if __name__ == "__main__":
     robot = Jackal(10)
     time = 0
+    tf = 12
     position_log = []
+    trajectory_log = []
     time_log = []
     v_log = []
     velocity_log = []
@@ -21,9 +23,10 @@ if __name__ == "__main__":
         
     #Robots position from inertial frame
     P0b = robot.getPosition()
+    print('First Position', P0b[:2])
 
     #Robots desired position from robots frame
-    Pd = np.array([1.5, 1.0, 0.0])
+    Pd = np.array([3.0, 1.2, 0.0])
     #Robots desired position from inertial frame
     P0d = P0b + changePerspective(Pd, R0b)
 
@@ -31,15 +34,22 @@ if __name__ == "__main__":
         while not rospy.is_shutdown():
             pos = robot.getPosition()
 
-            e = getError(pos, P0d, robot.getRotationMatrix()) # From robots frame
-            
-            pd, v, a = getFifthOrder(time, 8, P0b, 0, 0, P0d, 0, 0)
+            pd, v, a = getFifthOrder(time, tf, P0b, 0, 0, P0d, 0, 0)
             v = np.linalg.norm(v)
             a = np.linalg.norm(a)
 
-            ang_vel = 0.7 * getAngularError(e)            
-            if np.linalg.norm(e[:2]) < 0.05:
+            e = getError(pos, pd, robot.getRotationMatrix()) # From robots frame
+
+            lin_vel = v
+            ang_vel = 0
+
+            if time != 0:
                 ang_vel = 0
+                if time < tf:
+                    ang_vel = 0.6 * getAngularError(e)
+                lin_vel = 1 * getLinearError(e)
+
+            if time >= tf + 2:
                 raise Exception
 
             robot.setLinearSpeed(v)
@@ -51,8 +61,9 @@ if __name__ == "__main__":
             time += 0.1
 
             #Log Data
+            position_log.append(pos[:2])
+            trajectory_log.append(pd[:2])
             velocity_log.append(velocity)
-            position_log.append(pos)
             v_log.append(v)
             time_log.append(time)
 
@@ -64,6 +75,14 @@ if __name__ == "__main__":
     except Exception:
         print('Last Position: ', pos[:2])
         plt.figure()
+        plt.plot(np.array(time_log), np.array(position_log))
+        plt.plot(np.array(time_log), np.array(trajectory_log), linestyle='dashed')
+        plt.xlabel('Time')
+        plt.ylabel('Distance')
+        plt.legend(['x', 'y','trajectory x', 'trajectory y'])
+        plt.grid()
+
+        plt.figure()
         plt.plot(np.array(time_log), np.array(v_log))
         plt.plot(np.array(time_log), np.array(velocity_log))
         plt.xlabel('Time')
@@ -71,3 +90,5 @@ if __name__ == "__main__":
         plt.legend(['Commanded velocity', 'Calculated velocity'])
         plt.grid()
         plt.show()
+
+        print(np.linalg.norm(P0b[:2] - pos[:2]))
